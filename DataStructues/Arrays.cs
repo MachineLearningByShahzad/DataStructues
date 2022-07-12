3ba5123f01ca5106db048ec8dd4b2de904ab2bbc
Shahzad Ahmad <shahzad.ahmadcs49@gmail.com>
2022-07-12 15:34:45 +0500

    Add project files.

---
+++ DataStructues/Arrays.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructues
+{
+    internal class Arrays
+    {
+        private int[] array;
+        private int count = 0;
+
+        public Arrays(int length) { 
+            array = new int[length];
+        }
+
+        public void Print() {
+            for (int i = 0; i < count; i++) { 
+                Console.WriteLine(array[i]);
+            }
+        }
+
+        public void Insert(int a) {
+            //Resize the array if the items reached to the end
+            if (array.Length == count) {
+                int[] newArray = new int [count * 2];
+                for (int i = 0; i < count; i++){
+                    newArray[i] = array[i];
+                }
+                array = newArray;
+
+                
+            }
+            //Insert items at the end of the array
+            array[count++] = a;
+        }
+
+        public void removeAt(int index) {
+            // Validate the index
+            if (index >= 0 && index < count)
+            {
+                array[index] = 0;
+                //Shift the items to the left to fill the hole
+                for (int i = index; i < count; i++)
+                {
+                    array[i] = array[i + 1];
+                }
+                count--;
+            }
+            else { 
+                throw new IndexOutOfRangeException();
+            }
+        }
+
+        public int indexOf(int value) {
+            for (int i = 0; i < count; i++) {
+                if (array[i] == value) { 
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        public int max() {
+            int maxValue = 0;
+            for (int i = 0; i < count; i++) {
+                if (array[i] > maxValue) {
+                    maxValue = array[i];
+                }
+            }
+            return maxValue;
+        }
+
+        public int min()
+        {
+            int minValue = 10;
+            for (int i = 0; i < count; i++)
+            {
+                if (array[i] < minValue)
+                {
+                    minValue = array[i];
+                }
+            }
+            return minValue;
+        }
+
+        public int[] reverse() {
+            int[] a = new int[count];
+            int revCount = 0;
+            for(int i = count - 1; i >= 0; i--)
+            {
+                a[revCount] = array[i];
+                revCount++;
+            }
+            return a;
+        }
+    }
+}
---
+++ DataStructues/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DataStructues;
+
+//Array Data Structures
+Arrays item = new Arrays(3);
+item.Insert(1);
+item.Insert(2);
+item.Insert(3); 
+item.Insert(4); 
+item.Print();
+Console.WriteLine("Now removing from index of array");
+item.removeAt(2);
+item.Print();
+Console.WriteLine("Max Value in array:  " + item.max());
+Console.WriteLine("Min Value in array:  " + item.min());
+
+int[] revArray = item.reverse();
+for (int i= 0; i < revArray.Length; i++){
+    Console.WriteLine(revArray[i]);
+}
+
+//Link List Data Structures
